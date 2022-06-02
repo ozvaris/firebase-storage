3b67823196b7fc18c1685600f4a16a18e8271e4b
Ozgur <ozgurozvaris@hotmail.com>
2022-06-02 19:26:36 +0100

    bug fixed

--- src/App.js
+++ src/App.js
@@ -4,35 +4,38 @@ import { getStorage, ref, listAll } from 'firebase/storage';
 import app from './common/firebase';
 
 function App() {
-  const { listItems, setListItems } = useState([]);
+  const [listItems, setListItems] = useState(null);
+
   // Get a reference to the storage service, which is used to create references in your storage bucket
   const storage = getStorage(app, 'gs://b2bal-c1612.appspot.com');
 
   // Points to the root reference
   const storageRef = ref(storage);
 
-  listAll(storageRef)
-    .then((res) => {
-      res.prefixes.forEach((folderRef) => {
-        // All the prefixes under listRef.
-        // You may call listAll() recursively on them.
-      });
-      const items = res.items.map(
-        (item, i) =>
+  const click = () =>
+    listAll(storageRef)
+      .then((res) => {
+        res.prefixes.forEach((folderRef) => {
+          // All the prefixes under listRef.
+          // You may call listAll() recursively on them.
+        });
+        const items = res.items.map((item, i) => (
           //All the items under listRef.
-          item.name
-      );
-      console.log(items);
-      setListItems(items);
-    })
-    .catch((error) => {
-      // Uh-oh, an error occurred!
-    });
+          <li key={item.name}>{item.name}</li>
+        ));
+        console.log('a');
+        setListItems(items);
+      })
+      .catch((error) => {
+        // Uh-oh, an error occurred!
+      });
 
   return (
     <div className="App">
       <header className="App-header">
-        test
+        <button type="button" onClick={() => click()}>
+          Get List
+        </button>
         <ul>{listItems}</ul>
       </header>
     </div>
